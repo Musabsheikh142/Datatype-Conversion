fb3561a6d8bfe97d56dbb25f5592c54b27a02a37
Admin <Admin@MSK>
2022-09-11 11:25:35 +0530

    Add project files.

---
+++ Conversion/Program.cs
@@ -0,0 +1,46 @@
+﻿//implicit conversion & explicit conversion
+//implicit (Automatic)
+using System.Data;
+
+byte b = 255;
+
+Int16 i = b;
+Int32 i1 = i;
+Int64 i2 = i1;//implicit from lower to hinger
+//i = i1;//cannot convert from higher to lower
+
+
+//Explivit Method1 -- Adding Datatype before Veriable
+i1 = (Int32)i2;
+i = (Int16)i1;
+b = (byte)i;
+//Method2 using predefinded function called Method
+i1 = Convert.ToInt32(i2);
+i = Convert.ToInt16(i1);
+b = Convert.ToByte(i);
+
+//From 1 to 1.0 or 2.1 to 2
+float f = i1;//converted implicitly
+i1 = (int)f;//to be converted explivitly
+i1 = Convert.ToInt32(f);//1.3 --> 1 & 1.7 ->2
+
+Char c = 'a';//valued type & all the above are valued type variables
+int i3 = 'a';
+i3 = c;
+i1 = c;
+c = (char)i3;
+string str = "2";
+int i4 = Convert.ToInt32(str);
+
+str = Convert.ToString(i);
+str = i.ToString();
+str = 3.ToString();
+
+bool b1 = Convert.ToBoolean("True");
+b1 = Convert.ToBoolean("true");
+b1 = Convert.ToBoolean("TRUE");
+
+str = b1.ToString();
+str = Convert.ToString(b1);
+//Datetype. TryParse out keyword
+Console.WriteLine(i2);
---
+++ Conversion/TryParse Method.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    internal class TryParse_Method
+    {
+        void main()
+        {
+
+            //datatype.TryParse out keyword
+            //out keyword is part of Reference (Address), ref is also a keyword that holds reference
+
+
+            //tryParse
+           // using System.Collections.Immutable;
+
+            string str = "2";
+            //int i = Convert.ToInt32(str);
+            bool isSucceeded = Int32.TryParse(str, out int a);
+            Console.WriteLine($"Is Succeeded : {isSucceeded}, and value is {a}");
+
+            char c = 'A';
+            a = (int)c;
+            Console.WriteLine($"value is {a}");
+            isSucceeded = Int32.TryParse(c.ToString(), out a);
+            Console.WriteLine($"Is Succeeded : {isSucceeded}, and value is {a}");
+
+            isSucceeded = bool.TryParse("TRUEE", out bool result);
+            Console.WriteLine($"Is Succeeded : {isSucceeded}, and value is {result}");
+            isSucceeded = double.TryParse("2..33", out double resultDouble);
+            Console.WriteLine($"Is Succeeded : {isSucceeded}, and value is {resultDouble}");
+
+            try
+            {
+                Console.WriteLine("enter any number to convert into int");
+                a = Convert.ToInt32(Console.ReadLine());
+                isSucceeded = true;
+            }
+            catch (Exception ex)
+            {
+                isSucceeded = false;
+                a = 0;
+            }
+            finally
+            {
+                Console.WriteLine($"Is Succeeded : {isSucceeded}, and value is {a}");
+            }
+        }
+    }
+}
